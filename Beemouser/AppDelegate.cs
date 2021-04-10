c610266b7a0f1f59653d4364aa0227fa71aadcfe
Oğuzhan Öğreden <oguzhan@oguzhanogreden.com>
2021-04-10 11:40:30 +0200

    Add status bar item

--- Beemouser/AppDelegate.cs
+++ Beemouser/AppDelegate.cs
@@ -3,6 +3,7 @@ using AppKit;
 using Beemouser.DbContexts;
 using Beemouser.Domain.Models;
 using Beemouser.QuartzCoreHelpers;
+using CoreGraphics;
 using Foundation;
 
 namespace Beemouser
@@ -10,15 +11,60 @@ namespace Beemouser
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
     {
+        private NSStatusItem StatusItem { get; set; }
+
         public AppDelegate()
         {
+        }
+
+        private NSImage getStatusBarItemImage()
+        {
+            
+            var image = new NSImage()
+            {
+                Name = "AppIcon",
+                Size = new CGSize(16, 16)
+            };
+
+            return image;
+        }
+
+        private void showPreferences()
+        {
+            var storyboard = NSStoryboard.FromName("Main", null);
+            var controller = (NSViewController)storyboard.InstantiateControllerWithIdentifier("PreferencesController");
+            controller.PresentViewControllerAsModalWindow(controller);
+        }
+
+        private void makeStatusBarMenu()
+        {
+            var statusBar = NSStatusBar.SystemStatusBar;
+
+            var item = statusBar.CreateStatusItem(NSStatusItemLength.Variable);
+            item.Title = "BeeMouser";
+
+            item.Button.Activated += (sender, args) =>
+            {
+                showPreferences();
+            };
+
+            StatusItem = item;
         }
 
         public override void DidFinishLaunching(NSNotification notification)
         {
             // Insert code here to initialize your application
+            CaptureClicks();
 
-            // Do any additional setup after loading the view.
+            makeStatusBarMenu();
+        }
+
+        public override void WillTerminate(NSNotification notification) {
+            // Insert code here to tear down your application
+        }
+
+        private void CaptureClicks()
+        {
             NSEvent.AddGlobalMonitorForEventsMatchingMask(NSEventMask.LeftMouseDown, (NSEvent theEvent) =>
             {
                 var windowNumber = theEvent.WindowNumber;
@@ -36,10 +82,6 @@ namespace Beemouser
                     _context.SaveChanges();
                 }
             });
-        }
-        
-        public override void WillTerminate(NSNotification notification) {
-            // Insert code here to tear down your application
         }
         
         private string GetWindow(nint windowNumber) {
